a9cd11db54c8a927639419bab56faee507f680dd
serraangelaperez <serra.angelaperez@gmail.com>
2026-02-27 20:22:01 +0100

    Update cnn_simple.py

--- models/cnn_simple.py
+++ models/cnn_simple.py
@@ -6,8 +6,8 @@ def build_simple_cnn(input_shape=(227, 227, 3)):
         layers.Input(shape=input_shape),
         
         # Una sola capa convolucional
-        # Usamos 8 filtros de 3x3 para detectar bordes básicos
-        layers.Conv2D(8, (3, 3), activation='relu', padding='same'),
+        # Usamos 2 filtros de 3x3 para detectar bordes básicos
+        layers.Conv2D(2, (3, 3), activation='relu', padding='same'),
         
         # Global Average Pooling: Reduce cada mapa de activación a un solo número
         layers.GlobalAveragePooling2D(),
@@ -15,4 +15,5 @@ def build_simple_cnn(input_shape=(227, 227, 3)):
         # Capa de salida: 1 neurona para clasificación binaria
         layers.Dense(1, activation='sigmoid')
     ])
+
     return model
